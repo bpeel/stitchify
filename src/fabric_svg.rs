c8b7eec74e2141b37edc07ae1129b7ab8af5cd89
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-02-14 23:16:40 +0100

    fabric_svg: Move helper functions to methods of a class
    That way it doesn’t have to keep passing around all the data in
    arguments.

--- src/fabric_svg.rs
+++ src/fabric_svg.rs
@@ -22,353 +22,313 @@ use std::fmt::Write;
 const BOX_WIDTH: u16 = 20;
 const LINE_WIDTH: f32 = BOX_WIDTH as f32 / 6.0;
 
-fn generate_box(
+struct SvgGenerator<'a> {
     box_width: u16,
     box_height: u16,
-    x: u16,
-    y: u16,
-    color: Color,
-) -> XMLElement {
-    let mut path = XMLElement::new("path");
-
-    path.add_attribute(
-        "fill",
-        format!(
-            "rgb({}%, {}%, {}%)",
-            color[0] as f32 * 100.0 / 255.0,
-            color[1] as f32 * 100.0 / 255.0,
-            color[2] as f32 * 100.0 / 255.0,
-        ),
-    );
-
-    path.add_attribute(
-        "d",
-        format!(
-            "M {} {} l {} 0 l 0 {} l -{} 0 z",
-            x * box_width,
-            y * box_height,
-            box_width,
-            box_height,
-            box_width,
-        ),
-    );
-
-    path
+    fabric: &'a Fabric,
 }
 
-fn generate_boxes(
-    box_width: u16,
-    box_height: u16,
-    fabric: &Fabric,
-) -> XMLElement {
-    let mut group = XMLElement::new("g");
+impl<'a> SvgGenerator<'a> {
+    fn generate_box(
+        &self,
+        x: u16,
+        y: u16,
+        color: Color,
+    ) -> XMLElement {
+        let mut path = XMLElement::new("path");
 
-    group.add_attribute("id", "boxes");
+        path.add_attribute(
+            "fill",
+            format!(
+                "rgb({}%, {}%, {}%)",
+                color[0] as f32 * 100.0 / 255.0,
+                color[1] as f32 * 100.0 / 255.0,
+                color[2] as f32 * 100.0 / 255.0,
+            ),
+        );
 
-    for (stitch_num, stitch) in fabric.stitches().iter().enumerate() {
-        let x = stitch_num as u16 % fabric.n_stitches();
-        let y = stitch_num as u16 / fabric.n_stitches();
+        path.add_attribute(
+            "d",
+            format!(
+                "M {} {} l {} 0 l 0 {} l -{} 0 z",
+                x * self.box_width,
+                y * self.box_height,
+                self.box_width,
+                self.box_height,
+                self.box_width,
+            ),
+        );
 
-        group.add_child(generate_box(
-            box_width,
-            box_height,
-            x,
-            y,
-            stitch.color,
-        ));
+        path
     }
 
-    group
-}
+    fn generate_boxes(&self) -> XMLElement {
+        let mut group = XMLElement::new("g");
 
-fn generate_grid_no_id(
-    box_width: u16,
-    box_height: u16,
-    n_columns: u16,
-    n_rows: u16,
-) -> XMLElement {
-    let mut path = XMLElement::new("path");
+        group.add_attribute("id", "boxes");
 
-    path.add_attribute("stroke-width", LINE_WIDTH);
-    path.add_attribute("stroke-linecap", "square");
-    path.add_attribute("stroke-linejoin", "miter");
-    path.add_attribute("stroke", "rgb(71%, 71%, 71%)");
+        for (stitch_num, stitch) in self.fabric.stitches().iter().enumerate() {
+            let x = stitch_num as u16 % self.fabric.n_stitches();
+            let y = stitch_num as u16 / self.fabric.n_stitches();
 
-    let mut path_str = String::new();
-
-    for x in 0..=n_columns {
-        if x != 0 {
-            path_str.push(' ');
+            group.add_child(self.generate_box(x, y, stitch.color));
         }
 
-        write!(
-            &mut path_str,
-            "M {} 0 l 0 {}",
-            x * box_width,
-            n_rows * box_height,
-        ).unwrap();
+        group
     }
 
-    for y in 0..=n_rows {
-        if y != 0 {
-            path_str.push(' ');
-        }
+    fn generate_grid_no_id(&self, n_columns: u16, n_rows: u16) -> XMLElement {
+        let mut path = XMLElement::new("path");
 
-        write!(
-            &mut path_str,
-            "M 0 {} l {} 0",
-            y * box_height,
-            n_columns * box_width,
-        ).unwrap();
-    }
+        path.add_attribute("stroke-width", LINE_WIDTH);
+        path.add_attribute("stroke-linecap", "square");
+        path.add_attribute("stroke-linejoin", "miter");
+        path.add_attribute("stroke", "rgb(71%, 71%, 71%)");
 
-    path.add_attribute("d", path_str);
+        let mut path_str = String::new();
 
-    path
-}
-
-fn generate_grid(
-    box_width: u16,
-    box_height: u16,
-    n_columns: u16,
-    n_rows: u16,
-) -> XMLElement {
-    let mut path = generate_grid_no_id(
-        box_width,
-        box_height,
-        n_columns,
-        n_rows
-    );
-
-    path.add_attribute("id", "grid");
-
-    path
-}
-
-fn set_text_appearance(
-    element: &mut XMLElement,
-    box_height: u16,
-) {
-    element.add_attribute("font-family", "Sans");
-    element.add_attribute("font-size", box_height as f32 * 0.6);
-}
-
-fn set_text_position(
-    text: &mut XMLElement,
-    box_width: u16,
-    box_height: u16,
-    x: u16,
-    y: u16,
-) {
-    text.add_attribute("x", x as f32 + 0.5 * box_width as f32);
-    text.add_attribute("y", y as f32 + 0.7 * box_height as f32);
-    text.add_attribute("text-anchor", "middle");
-}
-
-fn generate_rulers(
-    box_width: u16,
-    box_height: u16,
-    n_columns: u16,
-    n_rows: u16,
-) -> XMLElement {
-    let mut group = XMLElement::new("g");
-
-    group.add_attribute("id", "rulers");
-
-    set_text_appearance(&mut group, box_height);
-
-    for y in 0..n_rows {
-        let mut text = XMLElement::new("text");
-
-        set_text_position(
-            &mut text,
-            box_width,
-            box_height,
-            n_columns * box_width,
-            y * box_height,
-        );
-
-        text.add_text(n_rows - y);
-
-        group.add_child(text);
-    }
-
-    for x in 0..n_columns {
-        let mut text = XMLElement::new("text");
-
-        set_text_position(
-            &mut text,
-            box_width,
-            box_height,
-            x * box_width,
-            n_rows * box_height,
-        );
-
-        text.add_text(n_columns - x);
-
-        group.add_child(text);
-    }
-
-    group
-}
-
-fn generate_box_thread_text(
-    thread: u16,
-    x: u16,
-    y: u16,
-    color: Color,
-) -> XMLElement {
-    let mut element = XMLElement::new("use");
-
-    element.add_attribute(
-        "xlink:href",
-        format!("#thread-{}", thread)
-    );
-    element.add_attribute("x", x);
-    element.add_attribute("y", y);
-
-    if color.iter().map(|&x| x as u16).sum::<u16>() < 384 {
-        element.add_attribute("fill", "rgb(100%, 100%, 100%)");
-    }
-
-    element
-}
-
-fn generate_box_threads(
-    box_width: u16,
-    box_height: u16,
-    fabric: &Fabric,
-) -> XMLElement {
-    let mut group = XMLElement::new("g");
-
-    group.add_attribute("id", "box-threads");
-
-    for (stitch_num, stitch) in fabric.stitches().iter().enumerate() {
-        let x = stitch_num as u16 % fabric.n_stitches();
-        let y = stitch_num as u16 / fabric.n_stitches();
-
-        group.add_child(generate_box_thread_text(
-            stitch.thread,
-            x * box_width,
-            y * box_height,
-            stitch.color,
-        ));
-    }
-
-    group
-}
-
-fn generate_thread_counts(
-    box_width: u16,
-    box_height: u16,
-    fabric: &Fabric,
-) -> XMLElement {
-    let mut group = XMLElement::new("g");
-
-    group.add_attribute("id", "thread-counts");
-
-    group.add_attribute(
-        "transform",
-        format!(
-            "translate({} {})",
-            box_width,
-            box_height * (fabric.n_rows() + 2),
-        ),
-    );
-
-    let mut counts = XMLElement::new("g");
-
-    set_text_appearance(&mut counts, box_height);
-
-    for (y, thread) in fabric.threads().iter().enumerate() {
-        group.add_child(generate_box(
-            box_width,
-            box_height,
-            0,
-            y as u16,
-            thread.color,
-        ));
-
-        group.add_child(generate_box_thread_text(
-            thread.id,
-            0,
-            y as u16 * box_height,
-            thread.color,
-        ));
-
-        let mut count_text = XMLElement::new("text");
-        set_text_position(
-            &mut count_text,
-            box_width,
-            box_height,
-            box_width,
-            y as u16 * box_height,
-        );
-        count_text.add_text(format!("{}", thread.stitch_count));
-        counts.add_child(count_text);
-    }
-
-    group.add_child(generate_grid_no_id(
-        box_width,
-        box_height,
-        1,
-        fabric.threads().len() as u16,
-    ));
-
-    group.add_child(counts);
-
-    group
-}
-
-fn generate_defs(
-    fabric: &Fabric,
-    box_width: u16,
-    box_height: u16,
-) -> XMLElement {
-    let mut defs = XMLElement::new("defs");
-
-    for thread in fabric.threads().iter() {
-        let text = if thread.id == 0 {
-            "A".to_string()
-        } else {
-            let mut parts = Vec::new();
-            let mut id = thread.id;
-
-            while id > 0 {
-                parts.push(
-                    char::from_u32('A' as u32 + id as u32 % 26).unwrap()
-                );
-                id /= 26;
+        for x in 0..=n_columns {
+            if x != 0 {
+                path_str.push(' ');
             }
 
-            parts.iter().rev().collect::<String>()
-        };
+            write!(
+                &mut path_str,
+                "M {} 0 l 0 {}",
+                x * self.box_width,
+                n_rows * self.box_height,
+            ).unwrap();
+        }
 
-        let mut element = XMLElement::new("text");
+        for y in 0..=n_rows {
+            if y != 0 {
+                path_str.push(' ');
+            }
 
-        set_text_appearance(&mut element, box_height);
-        set_text_position(&mut element, box_width, box_height, 0, 0);
+            write!(
+                &mut path_str,
+                "M 0 {} l {} 0",
+                y * self.box_height,
+                n_columns * self.box_width,
+            ).unwrap();
+        }
 
-        element.add_text(text);
-        element.add_attribute("id", format!("thread-{}", thread.id));
+        path.add_attribute("d", path_str);
 
-        defs.add_child(element);
+        path
     }
 
-    defs
+    fn generate_grid(&self) -> XMLElement {
+        let fabric = self.fabric;
+        let mut path = self.generate_grid_no_id(
+            fabric.n_stitches(),
+            fabric.n_rows()
+        );
+
+        path.add_attribute("id", "grid");
+
+        path
+    }
+
+    fn set_text_appearance(&self, element: &mut XMLElement) {
+        element.add_attribute("font-family", "Sans");
+        element.add_attribute("font-size", self.box_height as f32 * 0.6);
+    }
+
+    fn set_text_position(
+        &self,
+        text: &mut XMLElement,
+        x: u16,
+        y: u16,
+    ) {
+        text.add_attribute("x", x as f32 + 0.5 * self.box_width as f32);
+        text.add_attribute("y", y as f32 + 0.7 * self.box_height as f32);
+        text.add_attribute("text-anchor", "middle");
+    }
+
+    fn generate_rulers(&self) -> XMLElement {
+        let mut group = XMLElement::new("g");
+
+        group.add_attribute("id", "rulers");
+
+        self.set_text_appearance(&mut group);
+
+        for y in 0..self.fabric.n_rows() {
+            let mut text = XMLElement::new("text");
+
+            self.set_text_position(
+                &mut text,
+                self.fabric.n_stitches() * self.box_width,
+                y * self.box_height,
+            );
+
+            text.add_text(self.fabric.n_rows() - y);
+
+            group.add_child(text);
+        }
+
+        for x in 0..self.fabric.n_stitches() {
+            let mut text = XMLElement::new("text");
+
+            self.set_text_position(
+                &mut text,
+                x * self.box_width,
+                self.fabric.n_rows() * self.box_height,
+            );
+
+            text.add_text(self.fabric.n_stitches() - x);
+
+            group.add_child(text);
+        }
+
+        group
+    }
+
+    fn generate_box_thread_text(
+        &self,
+        thread: u16,
+        x: u16,
+        y: u16,
+        color: Color,
+    ) -> XMLElement {
+        let mut element = XMLElement::new("use");
+
+        element.add_attribute(
+            "xlink:href",
+            format!("#thread-{}", thread)
+        );
+        element.add_attribute("x", x);
+        element.add_attribute("y", y);
+
+        if color.iter().map(|&x| x as u16).sum::<u16>() < 384 {
+            element.add_attribute("fill", "rgb(100%, 100%, 100%)");
+        }
+
+        element
+    }
+
+    fn generate_box_threads(&self) -> XMLElement {
+        let mut group = XMLElement::new("g");
+
+        group.add_attribute("id", "box-threads");
+
+        for (stitch_num, stitch) in self.fabric.stitches().iter().enumerate() {
+            let x = stitch_num as u16 % self.fabric.n_stitches();
+            let y = stitch_num as u16 / self.fabric.n_stitches();
+
+            group.add_child(self.generate_box_thread_text(
+                stitch.thread,
+                x * self.box_width,
+                y * self.box_height,
+                stitch.color,
+            ));
+        }
+
+        group
+    }
+
+    fn generate_thread_counts(&self ) -> XMLElement {
+        let mut group = XMLElement::new("g");
+
+        group.add_attribute("id", "thread-counts");
+
+        group.add_attribute(
+            "transform",
+            format!(
+                "translate({} {})",
+                self.box_width,
+                self.box_height * (self.fabric.n_rows() + 2),
+            ),
+        );
+
+        let mut counts = XMLElement::new("g");
+
+        self.set_text_appearance(&mut counts);
+
+        for (y, thread) in self.fabric.threads().iter().enumerate() {
+            group.add_child(self.generate_box(
+                0,
+                y as u16,
+                thread.color,
+            ));
+
+            group.add_child(self.generate_box_thread_text(
+                thread.id,
+                0,
+                y as u16 * self.box_height,
+                thread.color,
+            ));
+
+            let mut count_text = XMLElement::new("text");
+            self.set_text_position(
+                &mut count_text,
+                self.box_width,
+                y as u16 * self.box_height,
+            );
+            count_text.add_text(format!("{}", thread.stitch_count));
+            counts.add_child(count_text);
+        }
+
+        group.add_child(self.generate_grid_no_id(
+            1,
+            self.fabric.threads().len() as u16,
+        ));
+
+        group.add_child(counts);
+
+        group
+    }
+
+    fn generate_defs(&self) -> XMLElement {
+        let mut defs = XMLElement::new("defs");
+
+        for thread in self.fabric.threads().iter() {
+            let text = if thread.id == 0 {
+                "A".to_string()
+            } else {
+                let mut parts = Vec::new();
+                let mut id = thread.id;
+
+                while id > 0 {
+                    parts.push(
+                        char::from_u32('A' as u32 + id as u32 % 26).unwrap()
+                    );
+                    id /= 26;
+                }
+
+                parts.iter().rev().collect::<String>()
+            };
+
+            let mut element = XMLElement::new("text");
+
+            self.set_text_appearance(&mut element);
+            self.set_text_position(&mut element, 0, 0);
+
+            element.add_text(text);
+            element.add_attribute("id", format!("thread-{}", thread.id));
+
+            defs.add_child(element);
+        }
+
+        defs
+    }
 }
 
 pub fn convert(dimensions: &Dimensions, fabric: &Fabric) -> XMLElement {
-    let box_height = BOX_WIDTH
-        * dimensions.gauge_stitches
-        / dimensions.gauge_rows;
+    let generator = SvgGenerator {
+        box_width: BOX_WIDTH,
+        box_height: BOX_WIDTH
+            * dimensions.gauge_stitches
+            / dimensions.gauge_rows,
+        fabric,
+    };
 
     let mut svg = XMLElement::new("svg");
 
     let svg_width = ((fabric.n_stitches() + 1) * BOX_WIDTH) as f32
         + LINE_WIDTH / 2.0;
     let svg_height = ((fabric.n_rows() + 2 + fabric.threads().len() as u16)
-                      * box_height) as f32
+                      * generator.box_height) as f32
         + LINE_WIDTH;
 
     svg.add_attribute("xmlns", "http://www.w3.org/2000/svg");
@@ -377,7 +337,7 @@ pub fn convert(dimensions: &Dimensions, fabric: &Fabric) -> XMLElement {
     svg.add_attribute("height", svg_height);
     svg.add_attribute("viewBox", format!("0 0 {} {}", svg_width, svg_height));
 
-    svg.add_child(generate_defs(fabric, BOX_WIDTH, box_height));
+    svg.add_child(generator.generate_defs());
 
     let mut translation = XMLElement::new("g");
     translation.add_attribute(
@@ -385,37 +345,15 @@ pub fn convert(dimensions: &Dimensions, fabric: &Fabric) -> XMLElement {
         format!("translate({} {})", LINE_WIDTH / 2.0, LINE_WIDTH / 2.0),
     );
 
-    translation.add_child(generate_boxes(
-        BOX_WIDTH,
-        box_height,
-        fabric,
-    ));
+    translation.add_child(generator.generate_boxes());
 
-    translation.add_child(generate_grid(
-        BOX_WIDTH,
-        box_height,
-        fabric.n_stitches(),
-        fabric.n_rows(),
-    ));
+    translation.add_child(generator.generate_grid());
 
-    translation.add_child(generate_rulers(
-        BOX_WIDTH,
-        box_height,
-        fabric.n_stitches(),
-        fabric.n_rows(),
-    ));
+    translation.add_child(generator.generate_rulers());
 
-    translation.add_child(generate_box_threads(
-        BOX_WIDTH,
-        box_height,
-        fabric,
-    ));
+    translation.add_child(generator.generate_box_threads());
 
-    translation.add_child(generate_thread_counts(
-        BOX_WIDTH,
-        box_height,
-        fabric,
-    ));
+    translation.add_child(generator.generate_thread_counts());
 
     svg.add_child(translation);
 
